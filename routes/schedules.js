3a57291ffcacbab9c4b6bb3b68351ad4d238ac02
frewwz <anaikoumuten@gmail.com>
2017-03-17 09:14:28 +0000

    空の予定日が作成されないように変更

--- routes/schedules.js
+++ routes/schedules.js
@@ -171,7 +171,7 @@ router.post('/:scheduleId', authenticationEnsurer, (req, res, next) => {
           }).then((candidates) => {
             // 追加されているかチェック
             const candidateNames = parseCandidateNames(req);
-            if (candidateNames) {
+            if (candidateNames && !(candidateNames.length === 1 && candidateNames[0] === '' )) {
               createCandidatesAndRedirect(candidateNames, schedule.scheduleId, res);
             } else {
               res.redirect('/schedules/' + schedule.scheduleId);
